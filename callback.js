8e69bda1f425ce3fac8980123e5060396fb6a2dd
ivan-voropaev <77105169+ivan-voropaev@users.noreply.github.com>
2021-12-14 18:58:44 +0700

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,6 +1,6 @@
 ﻿Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.DEBUG;
-new Oidc.UserManager({response_mode:'query'}).signinCallback().then(function(user) {
+new Oidc.UserManager({response_mode:'query'}).signinCallback(window.location.href).then(function(user) {
     var url = window.location.origin;
     window.location.replace(url + '/womanleader/lk/index');
 }).catch(function(err) {
