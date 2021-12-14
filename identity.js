8d294b5782cf56759d1c85edc7a29f2e70652b97
ivan-voropaev <77105169+ivan-voropaev@users.noreply.github.com>
2021-12-14 16:13:46 +0700

    Update identity.js

--- identity.js
+++ identity.js
@@ -5,25 +5,6 @@ Oidc.Log.logger = console;
 
 const url = window.location.origin;
 
-const settings = {
-    authority: 'https://localhost:44356',
-    client_id: 'rsv-landings-client',
-    redirect_uri: url + '/womanleader/callback/index',
-    post_logout_redirect_uri: url + '/womanleader/index',
-    response_type: 'code',
-    scope: 'openid profile InternalApi',
-    silent_redirect_uri: url + '/womanleader/silent/index',
-    automaticSilentRenew:false,
-    validateSubOnSilentRenew: true,
-    monitorAnonymousSession : true,
-    filterProtocolClaims: true,
-    loadUserInfo: false,
-    revokeAccessTokenOnSignout : true,
-    //participation_uri: "https://participation.rsv-test.bizml.ru",
-    participation_uri: "http://localhost:8080",
-    project_id: "273"
-};
-
 const mgr = new Oidc.UserManager(settings);
 
 $(document).ready(() => {
